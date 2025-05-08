c03af61d414ad86786d838815873c3c8a9251c0a
tanzhijian <i@tanzhijian.org>
2025-05-09 00:12:08 +0800

    Update project dependencies and remove unused matplotlib visualizations

--- src/that_game/_visualization.py
+++ src/that_game/_visualization.py
@@ -1,10 +1,4 @@
-from typing import Sequence, cast
-
-import matplotlib.pyplot as plt
-from matplotlib.axes import Axes
-from mplsoccer import Pitch as MPLPitchLib
-from mplsoccer import VerticalPitch as MPLVerticalPitchLib
-from mplsoccer._pitch_plot import BasePitchPlot
+from typing import Sequence
 
 from ._models import Location, Pitch, Shot, Team
 
@@ -17,43 +11,12 @@ class _Colors:
     line = "#198754"
 
 
-def _mpl_pitch_factory(pitch: Pitch) -> BasePitchPlot:
-    if pitch.vertical:
-        Lib = MPLVerticalPitchLib
-    else:
-        Lib = MPLPitchLib
-    return Lib(
-        pitch_type="custom",
-        pitch_length=pitch.length,
-        pitch_width=pitch.width,
-        axis=True,
-        label=True,
-        pitch_color=_Colors.background,
-        line_color=_Colors.divide,
-    )
-
-
-def _create_mpl_pitch_and_ax(
-    pitch: Pitch,
-) -> tuple[BasePitchPlot, Axes]:
-    mpl_pitch = _mpl_pitch_factory(pitch)
-    _, ax = mpl_pitch.draw()
-    ax = cast(Axes, ax)
-
-    if pitch.length_direction == "left":
-        ax.invert_xaxis()
-    if pitch.width_direction == "down":
-        ax.invert_yaxis()
-    return mpl_pitch, ax
-
-
 class PitchVisualization:
     def __init__(self, pitch: Pitch) -> None:
         self.pitch = pitch
 
     def show(self) -> None:
-        _create_mpl_pitch_and_ax(self.pitch)
-        plt.show()
+        pass
 
 
 class LocationVisualization:
@@ -61,9 +24,7 @@ class LocationVisualization:
         self.location = location
 
     def show(self) -> None:
-        mpl_pitch, ax = _create_mpl_pitch_and_ax(self.location.pitch)
-        mpl_pitch.scatter(x=self.location.x, y=self.location.y, s=100, ax=ax)
-        plt.show()
+        pass
 
 
 class ShotVisualization:
@@ -77,60 +38,7 @@ class ShotVisualization:
         return _Colors.home_team
 
     def show(self) -> None:
-        mpl_pitch, ax = _create_mpl_pitch_and_ax(self.shot.location.pitch)
-        mpl_pitch.scatter(
-            x=self.shot.location.x,
-            y=self.shot.location.y,
-            s=100,
-            color=self._team_color,
-            ax=ax,
-        )
-
-        if (jersey_number := self.shot.player.jersey_number) is not None:
-            mpl_pitch.annotate(
-                text=jersey_number,
-                xy=(
-                    self.shot.location.x + 0.2,
-                    self.shot.location.y + 0.2,
-                ),
-                ax=ax,
-                color="white",
-                ha="center",
-                va="center",
-                fontsize=7,
-            )
-
-        mpl_pitch.lines(
-            self.shot.location.x,
-            self.shot.location.y,
-            self.shot.end_location.x,
-            self.shot.end_location.y,
-            color=_Colors.line,
-            lw=1,
-            ax=ax,
-        )
-
-        if self.shot.related_players is not None:
-            for player in self.shot.related_players:
-                if player.team.name == self.shot.team.name:
-                    mpl_pitch.scatter(
-                        x=player.location.x,
-                        y=player.location.y,
-                        s=100,
-                        ax=ax,
-                        color=_Colors.home_team,
-                        alpha=0.7,
-                    )
-                else:
-                    mpl_pitch.scatter(
-                        x=player.location.x,
-                        y=player.location.y,
-                        s=100,
-                        ax=ax,
-                        color=_Colors.away_team,
-                        alpha=0.7,
-                    )
-        plt.show()
+        pass
 
 
 class ShotsVisualization:
@@ -149,25 +57,7 @@ class ShotsVisualization:
         return _Colors.away_team
 
     def show(self) -> None:
-        mpl_pitch, ax = _create_mpl_pitch_and_ax(self.shots[0].location.pitch)
-        for shot in self.shots:
-            mpl_pitch.scatter(
-                x=shot.location.x,
-                y=shot.location.y,
-                s=100,
-                ax=ax,
-                color=self._team_color(shot.team),
-            )
-            mpl_pitch.lines(
-                shot.location.x,
-                shot.location.y,
-                shot.end_location.x,
-                shot.end_location.y,
-                color=_Colors.line,
-                lw=1,
-                ax=ax,
-            )
-        plt.show()
+        pass
 
     def xg(self) -> None:
         return
